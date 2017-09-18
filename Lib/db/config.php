228e4cb2d26000f7fe58f21fa2d4670b7b63a8d3
徐淑峰1 <shufeng.xu@xuesongwan.com>
2017-09-18 15:46:00 +0800

    PDO 封装类添加

---
+++ Lib/db/config.php
@@ -0,0 +1,10 @@
+<?php
+$dbConfig = array(
+    'dbHost'=>'localhost',
+    'dbUserName'=>'root',
+    'dbPassword'=>'root',
+    'dbName'=>'test',
+    'dbPort'=>'3306',
+    'dbType'=>'mysql',
+    'dbCharset'=>'utf8'
+);
