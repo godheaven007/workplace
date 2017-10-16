e0591ab6c2adf95831309a7ead426fe3918ee983
徐淑峰1 <shufeng.xu@xuesongwan.com>
2017-10-16 10:34:58 +0800

    pop plugin init

---
+++ plugins/pop/pop.js
@@ -0,0 +1,19 @@
+/**
+ * author: xusf
+ * createDate: 2017-10-16
+ * description: 弹框插件
+ */
+
+;(function($){
+    var Pop = function(){
+
+    };
+
+    Pop.prototype = {
+
+    };
+
+    $.fn.alert = function(){
+
+    }
+})(jQuery);
