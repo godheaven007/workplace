de5260bc32d60723197463753f3a2ac95aea5890
godheaven_007 <345777292@qq.com>
2018-09-06 22:50:51 +0800

    动态路由

---
+++ Vue/vue-router/src/components/User.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>{{$route.params.username}} - {{$route.params.sex}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  updated () {
+  }
+}
+</script>
+<style scoped>
+</style>
