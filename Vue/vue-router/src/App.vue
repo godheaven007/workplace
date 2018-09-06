d3e376de79fac05a6a70b4bb8b6a934ca18a7d76
godheaven_007 <345777292@qq.com>
2018-09-06 21:05:27 +0800

    普通路由

--- Vue/vue-router/src/App.vue
+++ Vue/vue-router/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-link to="/">首页</router-link> |
+    <router-link to="/about">关于</router-link>
+    <!-- 匹配到的组件渲染至此 -->
+    <router-view></router-view>
   </div>
 </template>
 
@@ -12,12 +14,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
