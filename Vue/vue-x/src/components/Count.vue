c78f46548f070e0385c473cbda0a53b61e12cde2
godheaven_007 <345777292@qq.com>
2018-09-09 14:20:52 +0800

    状态取值的五种方式

---
+++ Vue/vue-x/src/components/Count.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>{{getCount}}</h1>
+    <p>
+      <button @click="add">+</button>
+      <button @click="reduce">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import store from '@/store/index'
+import {mapState} from 'vuex'
+export default {
+  name: 'Count',
+  data () {
+    return {
+      msg: 'hello'
+    }
+  },
+  store,
+  methods: {
+    add () {
+      this.$store.commit('add')
+    },
+    reduce () {
+      this.$store.commit('reduce')
+    }
+  },
+  // 状态取值方式一
+  // computed: {
+  //   count () {
+  //     return this.$store.state.count
+  //   }
+  // }
+  // 状态取值方式二
+  // computed: mapState({
+  //   count: state => state.count
+  // })
+
+  // 状态取值方式三
+  // computed: mapState(['count'])
+
+  // 状态取值方式四
+  // computed: {
+  //   ...mapState(['count'])
+  // }
+
+  // 状态取值方式五
+  computed: {
+    ...mapState({
+      getCount: 'count'
+    })
+  }
+}
+</script>
