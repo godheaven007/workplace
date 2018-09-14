ca180f56c8768fbf328ad016570a798b59dfe18c
godheaven_007 <345777292@qq.com>
2018-09-14 08:29:30 +0800

    project init

--- Vue/vue-music/src/App.vue
+++ Vue/vue-music/src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <div id="app" @touchmove.prevent>
-    <m-header></m-header>
-    <tab></tab>
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
-    <player></player>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
-<script type="text/ecmascript-6">
-  import MHeader from 'components/m-header/m-header'
-  import Player from 'components/player/player'
-  import Tab from 'components/tab/tab'
-
-  export default {
-    components: {
-      MHeader,
-      Tab,
-      Player
-    }
-  }
+<script>
+export default {
+  name: 'App'
+}
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
